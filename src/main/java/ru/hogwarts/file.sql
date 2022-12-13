cc8fe4391c06c03e74348128be6c166812b4c5d8
Homework1 <danisholha760@gmail.com>
2022-12-12 22:09:00 -0600

    eighth commit

---
+++ src/main/java/ru/hogwarts/file.sql
@@ -0,0 +1,13 @@
+select * from student;
+
+select * from faculty;
+
+select * from student where age between 13 and 15;
+
+select name from student;
+
+select * from student where name like '%o%';
+
+select * from student where age < id;
+
+select * from student order by age, name;
